8bc124bb980c543ca065cd73bb010de7f7fe739b
Morgan Wall <mwall1992@gmail.com>
2014-06-29 08:06:20 +1000

    Restructured layout of app

---
+++ Split Britches Capture/Classes/View Controllers/UploadManagerTableViewController/SBCUploadManagerTableViewController.h
@@ -0,0 +1,15 @@
+/**
+ * @filename SBCUploadManagerTableViewController.h
+ * @author Morgan Wall
+ * @date 29-6-2014
+ * @copyright Copyright 2014 Split Britches. All rights reserved.
+ *
+ * @brief The table view controller responsible for managing file uploads to
+ * Split Britches' external data server.
+ */
+
+#import <UIKit/UIKit.h>
+
+@interface SBCUploadManagerTableViewController : UITableViewController
+
+@end
